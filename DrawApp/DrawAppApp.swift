fe9db362053bdcaf888681fc643bc2a4ad23170f
batuhanarat <arat_batuhan@windowslive.com>
2023-01-04 17:42:35 +0300

    Initial Commit

---
+++ DrawApp/DrawAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawAppApp.swift
+//  DrawApp
+//
+//  Created by Batuhan Arat on 4.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
